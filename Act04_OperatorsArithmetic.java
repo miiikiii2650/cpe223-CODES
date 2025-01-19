a644adc4b31812ba4db326d50c83e3fc4ab84f93
miiikiii2650 <hungryburger2650@gmail.com>
2025-01-19 14:46:21 +0800

    Add files via upload

---
+++ Act04_OperatorsArithmetic.java
@@ -0,0 +1,55 @@
+public class Act05_OperatorsArithmetic {
+    public static void main(String[] args) {
+        int a = 8, b = 5, x = 10, y = 3;
+        
+        //Variables
+        System.out.println("Variables...");
+        a = 8;
+        b = 5;
+        x = 10;
+        y = 3;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println(" ");
+
+        //Exponent
+        System.out.println("Exponentation...");
+        System.out.println("a^b = " + Math.pow(a,b));
+        System.out.println("x^y = " + Math.pow(x,y));
+        System.out.println(" ");
+
+        //Multiply
+        System.out.println("Multiplying...");
+        System.out.println("a * b = " + (a * b));
+        System.out.println("x * y = " + (x * y));
+        System.out.println(" ");
+
+        //Divide
+        System.out.println("Dividing...");
+        System.out.println("a / b = " + (a / b));
+        System.out.println("x / y = " + (x / y));
+        System.out.println(" ");
+
+        //Add
+        System.out.println("Adding...");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("x + y = " + (x + y));
+        System.out.println(" ");
+
+        //Subtract
+        System.out.println("Subtracting...");
+        System.out.println("a - b = " + (a - b));
+        System.out.println("x - y = " + (x - y));
+        System.out.println(" ");
+
+        //Remainder
+        System.out.println("Finding the remainder...");
+        System.out.println("a % b = " + (a % b));
+        System.out.println("x % y = " + (x % y));
+        System.out.println(" ");
+    
+
+    }
+}
